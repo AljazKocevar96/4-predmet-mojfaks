4b89f0b023d06b83183f55f9d7f6d8d4172db576
AljazKocevar96 <gem update rails --no-ri --no-rdoc>
2015-04-13 20:53:37 +0200

    Dodan profil

---
+++ profil.php
@@ -0,0 +1,56 @@
+<?php
+include_once 'db.php';
+include_once 'header.php';
+include_once 'funkcije.php';
+include_once 'session.php';
+?>
+
+<div class="col-lg-12 bg">
+    <div class="row">
+        <h3 style="color:rgb(34, 194, 34); margin-left: 3%;">Profil</h3>
+    </div>
+    <hr>
+ 
+   
+       
+            <div class="col-lg-4">
+                Podatki
+            </div>
+            <div class="col-lg-1">
+                br
+            </div>
+            
+            <div class="col-lg-7" style="float:right; " >
+                PriljubljenePriljubljenePriljubljenePriljubljenePriljubljene
+                    PriljubljenePriljubljenePriljubljenePriljubljenePriljubljenePriljubljene
+                    PriljubljenePriljubljenePriljubljenePriljubljenePriljubljenePriljubljene
+                    PriljubljenePriljubljenePriljubljenePriljubljenePriljubljenePriljubljene
+                    PriljubljenePriljubljenePriljubljenePriljubljenePriljubljenePriljubljene
+                    PriljubljenePriljubljenePriljubljenePriljubljenePriljubljenePriljubljenePri
+                    ljubljenePriljubljenePriljubljenePriljubljenePriljubljenePriljubljenePriljubljenePriljubljene
+            </div>
+       
+    <div class="col-lg-4" style="float: left; ">
+        neki
+    </div>
+    
+</div>
+
+
+<style>
+       .col-lg-12{
+        
+    }
+    
+    .bg{
+        border-top-style: solid ;
+        border-top-color: rgb(34, 194, 34); 
+        background-color: white;
+        box-shadow: 1px 1px 2px #bbbbbb;
+        margin:0 auto 0 5%; 
+        position:relative; 
+        width: 90%; 
+        
+    }
+</style>
+
